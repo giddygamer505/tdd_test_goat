679b0395608e0ed4b560a4e4469232e4485779c1
bl4ckmarchh <xvvsti@gmail.com>
2026-01-27 03:04:55 +0700

    FT priority input

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -56,25 +56,29 @@ class NewVisitorTest(LiveServerTestCase):
         # She is invited to enter a to-do item straight away
         # แก้ไข 4: ใช้ By.ID
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
 
         # She types "Buy peacock feathers" into a text box
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys("Buy peacock feathers")
+
+        #She see item priority box and add priority
+        input_pritority = self.browser.find_element(By.ID, 'id_priority_item')
+        self.assertEqual(input_pritority.get_attribute('placeholder'),'Enter an item priority')
+        input_pritority.send_keys("Medium")
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
-        inputbox.send_keys(Keys.ENTER)
+        input_pritority.submit()
         self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
         inputbox = self.browser.find_element(By.ID, "id_new_item")
+        input_pritority = self.browser.find_element(By.ID, 'id_priority_item')
         inputbox.send_keys("Use peacock feathers to make a fly")
-        inputbox.send_keys(Keys.ENTER)
+        input_pritority.send_keys("Low")
+        input_pritority.submit()
 
         # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
@@ -84,6 +88,3 @@ class NewVisitorTest(LiveServerTestCase):
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
         self.fail('Finish the test!')
-
-if __name__ == '__main__':
-    unittest.main()
