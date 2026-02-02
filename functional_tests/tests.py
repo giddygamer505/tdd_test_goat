8df5c97506c8371db87b123d07169132cf79c40f
bl4ckmarchh <xvvsti@gmail.com>
2026-02-03 02:18:23 +0700

    Hack FT runner to be able to test docker

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.common.exceptions import WebDriverException
-import unittest
+import os
 
 MAX_WAIT = 5  
 
@@ -16,6 +16,8 @@ options.binary_location = "/snap/firefox/current/usr/lib/firefox/firefox"
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox(options=options)
+        if test_server := os.environ.get("TEST_SERVER"):   
+            self.live_server_url = "http://" + test_server 
 
     def tearDown(self):
         self.browser.quit()
@@ -40,48 +42,43 @@ class NewVisitorTest(StaticLiveServerTestCase):
                     raise  
                 time.sleep(0.5)
 
-    # def test_can_start_a_todo_list(self):
-    #     # Edith has heard about a cool new online to-do app. She goes
-    #     # to check out its homepage
-    #     self.browser.get(self.live_server_url)
+    def test_can_start_a_todo_list(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get(self.live_server_url)
 
-    #     # She notices the page title and header mention to-do lists
-    #     self.assertIn('To-Do', self.browser.title)
-    #     header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
-    #     self.assertIn('To-Do', header_text)
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
 
-    #     # She is invited to enter a to-do item straight away
+        # She is invited to enter a to-do item straight away
 
-    #     inputbox = self.browser.find_element(By.ID, 'id_new_item')
-    #     self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
 
-    #     # She types "Buy peacock feathers" into a text box
-    #     inputbox.send_keys("Buy peacock feathers")
-
-    #     #Then she see add button
-    #     submit_button = self.browser.find_element(By.ID, 'id_submit')
+        # She types "Buy peacock feathers" into a text box
+        inputbox.send_keys("Buy peacock feathers")
         
-    #     # When she hits enter, the page updates, and now the page lists
-    #     # "1: Buy peacock feathers (Priority: Medium)" as an item in a to-do list table
-    #     submit_button.click()
-    #     self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers "" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
-    #     # There is still a text box inviting her to add another item.
-    #     # She enters "Use peacock feathers to make a fly"
-    #     # (Edith is very methodical)
-    #     inputbox = self.browser.find_element(By.ID, "id_new_item")
-    #     submit_button = self.browser.find_element(By.ID, 'id_submit')
-    #     inputbox.send_keys("Use peacock feathers to make a fly")
-    #     submit_button.click()
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathers to make a fly"
+        # (Edith is very methodical)
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
 
-    #     # The page updates again, and now shows both items on her list
-    #     self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
-    #     self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        # The page updates again, and now shows both items on her list
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
-    #     # Edith wonders whether the site will remember her list. Then she sees
-    #     # that the site has generated a unique URL for her -- there is some
-    #     # explanatory text to that effect.
-    #     self.fail('Finish the test!')
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
