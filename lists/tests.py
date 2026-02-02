e357e5076f74de4c1c80bf2ed4b52abdef2395b7
bl4ckmarchh <xvvsti@gmail.com>
2026-02-03 01:35:28 +0700

    Use Bootstrap to improve layout

--- lists/tests.py
+++ lists/tests.py
@@ -10,7 +10,11 @@ class HomePageTest(TestCase):
     def test_renders_input_form(self):
         response = self.client.get("/")
         self.assertContains(response, '<form method="POST" action="/lists/new">')
-        self.assertContains(response, '<input name="item_text"')
+        self.assertContains(
+            response,
+            '<input name="item_text" id="id_new_item" placeholder="Enter a to-do item" />',
+            html=True,
+        )
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
@@ -41,7 +45,11 @@ class ListViewTest(TestCase):
             response,
             f'<form method="POST" action="/lists/{mylist.id}/add_item">',
         )
-        self.assertContains(response, '<input name="item_text"')
+        self.assertContains(
+            response,
+            '<input name="item_text" id="id_new_item" placeholder="Enter a to-do item" />',
+            html=True,
+        )
 
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()  
