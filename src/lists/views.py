8133c693e73ee009ec4d607f578bbec76dfc0c1b
bl4ckmarchh <xvvsti@gmail.com>
2026-02-05 11:47:33 +0700

    Priority fix

--- src/lists/views.py
+++ src/lists/views.py
@@ -15,5 +15,5 @@ def view_list(request, list_id):
 
 def add_item(request, list_id):
     our_list = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST["item_text"], list=our_list)
+    Item.objects.create(text=request.POST["item_text"], list=our_list,priority=request.POST['priority_text'])
     return redirect(f"/lists/{our_list.id}/")
