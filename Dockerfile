f10f14d623e4cdbff100938f8cad0ced9b37da00
bl4ckmarchh <xvvsti@gmail.com>
2026-02-19 06:21:30 +0700

    docker multicontainer

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
-FROM python:3.12-slim
+FROM python:3.12-slim 
+
+RUN apt-get update && apt-get install -y \
+    gcc \
+    default-libmysqlclient-dev \
+    pkg-config \
+    && rm -rf /var/lib/apt/lists/*
 
 RUN python -m venv /venv  
 ENV PATH="/venv/bin:$PATH"  
 
-RUN pip install "django<6" 
+RUN pip install --upgrade pip
+RUN pip install "django<6" mysqlclient
 
 COPY src /src
-
 WORKDIR /src
 
-# หาบรรทัดสุดท้ายที่เขียนว่า CMD ... แล้วแก้เป็น:
-CMD sh -c "python manage.py migrate && python manage.py runserver 0.0.0.0:8888"
+CMD sh -c "sleep 10 && python manage.py migrate && python manage.py runserver 0.0.0.0:8888"
--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = ['https://*.koyeb.app']
 
 # Application definition
 
@@ -75,9 +76,17 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_db',                 # ตรงกับ MYSQL_DATABASE
+        'USER': 'user123',               # ตรงกับ MYSQL_USER 
+        'PASSWORD': 'password123',       # ตรงกับ MYSQL_PASSWORD
+        'HOST': 'db',                    # ใช้ชื่อ service "db" แทน localhost
+        'PORT': '3306',                  # Port มาตรฐานของ MySQL
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -119,4 +128,3 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "static"
 
-CSRF_TRUSTED_ORIGINS = ['https://*.koyeb.app']
